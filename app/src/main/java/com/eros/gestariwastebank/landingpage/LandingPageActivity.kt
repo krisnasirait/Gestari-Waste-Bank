95e56f6dbcdc3cef903139caeb135432698e94e8
Krisna Sirait <krisnapande00@outlook.com>
2022-11-03 15:08:46 +0700

    added function in LandingPageActivity.kt to show fragment

--- app/src/main/java/com/eros/gestariwastebank/landingpage/LandingPageActivity.kt
+++ app/src/main/java/com/eros/gestariwastebank/landingpage/LandingPageActivity.kt
@@ -1,16 +1,39 @@
 package com.eros.gestariwastebank.landingpage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.eros.gestariwastebank.R
+import androidx.fragment.app.Fragment
+import com.eros.gestariwastebank.databinding.ActivityLandingPageBinding
+import com.eros.gestariwastebank.landingpage.adapter.ViewPagerAdapter
+import com.eros.gestariwastebank.landingpage.fragmentlanding.FirstLandingFragment
+import com.eros.gestariwastebank.landingpage.fragmentlanding.SecondLandingFragment
+import com.eros.gestariwastebank.landingpage.fragmentlanding.ThirdLandingFragment
 
 class LandingPageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLandingPageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         val splashScreen = installSplashScreen()
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing_page)
+        binding = ActivityLandingPageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setFragment()
+    }
+
+    private fun setFragment() {
+        val fragment: ArrayList<Fragment> = arrayListOf(
+            FirstLandingFragment(),
+            SecondLandingFragment(),
+            ThirdLandingFragment()
+        )
+        ViewPagerAdapter(fragment, this).also {
+            binding.vpLandingPage.adapter = it
+        }
+        binding.circleIndicator.setViewPager(binding.vpLandingPage)
     }
 }
