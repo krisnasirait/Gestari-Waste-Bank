001bb7b3f52a94445a1309497780c41af2ea16cb
Krisna Sirait <krisnapande00@outlook.com>
2022-11-03 15:04:45 +0700

    create function for ViewPagerAdapter.kt

--- app/src/main/java/com/eros/gestariwastebank/landingpage/adapter/ViewPagerAdapter.kt
+++ app/src/main/java/com/eros/gestariwastebank/landingpage/adapter/ViewPagerAdapter.kt
@@ -1,4 +1,19 @@
 package com.eros.gestariwastebank.landingpage.adapter
 
-class ViewPagerAdapter {
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter (
+    private val items: ArrayList<Fragment>,
+    activity: AppCompatActivity
+) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return items[position]
+    }
+
 }
