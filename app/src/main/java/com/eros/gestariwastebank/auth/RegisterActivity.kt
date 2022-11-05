aaa8cd3d3e27ed17fb560b283e7d386569596d80
Krisna Sirait <krisnapande00@outlook.com>
2022-11-05 17:41:40 +0700

    added function to go to login in register

--- app/src/main/java/com/eros/gestariwastebank/auth/RegisterActivity.kt
+++ app/src/main/java/com/eros/gestariwastebank/auth/RegisterActivity.kt
@@ -1,12 +1,24 @@
 package com.eros.gestariwastebank.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.eros.gestariwastebank.R
+import com.eros.gestariwastebank.databinding.ActivityLoginBinding
+import com.eros.gestariwastebank.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.tvLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
