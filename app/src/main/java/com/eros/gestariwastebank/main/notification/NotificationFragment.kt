5e6859be86dfb4f0afa9592c20ada7664c04d9e3
Krisna Sirait <krisnapande00@outlook.com>
2022-11-09 15:10:42 +0700

    added layout for fragment notification

--- app/src/main/java/com/eros/gestariwastebank/main/notification/NotificationFragment.kt
+++ app/src/main/java/com/eros/gestariwastebank/main/notification/NotificationFragment.kt
@@ -6,15 +6,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.eros.gestariwastebank.R
+import com.eros.gestariwastebank.databinding.FragmentHomeBinding
+import com.eros.gestariwastebank.databinding.FragmentNotificationBinding
+import com.eros.gestariwastebank.main.notification.notification.NotificationAdapter
 
 
 class NotificationFragment : Fragment() {
+
+    private lateinit var binding: FragmentNotificationBinding
+    private lateinit var notificationAdapter: NotificationAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false)
+        binding = FragmentNotificationBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        notificationAdapter = NotificationAdapter()
     }
 }
