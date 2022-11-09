699f0ed35df546734de46a796bafaf102298ba9e
Krisna Sirait <krisnapande00@outlook.com>
2022-11-09 12:00:35 +0700

    moved to a new package

--- app/src/main/java/com/eros/gestariwastebank/main/wallet/WalletFragment.kt
+++ app/src/main/java/com/eros/gestariwastebank/main/wallet/WalletFragment.kt
@@ -1,10 +1,11 @@
-package com.eros.gestariwastebank
+package com.eros.gestariwastebank.main.wallet
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.eros.gestariwastebank.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
