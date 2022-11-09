f01aa9eb9b6fdc87339e134fa84c4ff01c12e41b
Krisna Sirait <krisnapande00@outlook.com>
2022-11-09 21:03:35 +0700

    added adapter for history

--- app/src/main/java/com/eros/gestariwastebank/main/wallet/history/HistoryAdapter.kt
+++ app/src/main/java/com/eros/gestariwastebank/main/wallet/history/HistoryAdapter.kt
@@ -1,8 +1,46 @@
 package com.eros.gestariwastebank.main.wallet.history
 
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.eros.gestariwastebank.databinding.ItemHistoryBinding
 
 class HistoryAdapter : RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
 
+    private lateinit var binding: ItemHistoryBinding
+    private val listHistory = mutableListOf<History>()
+
+    inner class ViewHolder(private val itemBinding: ItemHistoryBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(history: History) {
+            itemBinding.tvStatus.text = history.status
+            itemBinding.tvDate.text = history.tanggal
+            itemBinding.tvAmountMoney.text = history.amount.toString()
+            if(itemBinding.tvStatus.text == "Penarikan") {
+                itemBinding.tvAmountMoney.setTextColor(Color.RED)
+            } else {
+                itemBinding.tvAmountMoney.setTextColor(Color.GREEN)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        binding = ItemHistoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(listHistory[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listHistory.size
+    }
+
+    fun setData(item: List<History>) {
+        listHistory.addAll(item)
+        notifyDataSetChanged()
+    }
 
 }
