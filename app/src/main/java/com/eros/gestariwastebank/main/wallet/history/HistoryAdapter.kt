3c891f61b0fa4bd12991dbad31d9b6457c853841
Krisna Sirait <krisnapande00@outlook.com>
2022-11-09 20:53:49 +0700

    added layout for item history

---
+++ app/src/main/java/com/eros/gestariwastebank/main/wallet/history/HistoryAdapter.kt
@@ -0,0 +1,8 @@
+package com.eros.gestariwastebank.main.wallet.history
+
+import androidx.recyclerview.widget.RecyclerView
+
+class HistoryAdapter : RecyclerView.Adapter<HistoryAdapter.ViewHolder>() {
+
+
+}
