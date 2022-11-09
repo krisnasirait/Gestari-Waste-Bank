42e2422301ecca02ee55b33cbe5e92144ab6b602
Krisna Sirait <krisnapande00@outlook.com>
2022-11-09 15:13:04 +0700

    working fragment notification

--- app/src/main/java/com/eros/gestariwastebank/main/notification/NotificationFragment.kt
+++ app/src/main/java/com/eros/gestariwastebank/main/notification/NotificationFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.eros.gestariwastebank.R
+import com.eros.gestariwastebank.data.Util
 import com.eros.gestariwastebank.databinding.FragmentHomeBinding
 import com.eros.gestariwastebank.databinding.FragmentNotificationBinding
 import com.eros.gestariwastebank.main.notification.notification.NotificationAdapter
@@ -29,5 +31,16 @@ class NotificationFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         notificationAdapter = NotificationAdapter()
+
+        binding.rvNotification.adapter = notificationAdapter
+
+        binding.rvNotification.layoutManager =
+            LinearLayoutManager(
+                context,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+
+        notificationAdapter.setData(Util.allNotification)
     }
 }
