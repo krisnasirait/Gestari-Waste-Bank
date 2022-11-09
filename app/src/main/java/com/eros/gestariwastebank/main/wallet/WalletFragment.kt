557e31da148fd226fa6b08cac7edc3427640291a
Krisna Sirait <krisnapande00@outlook.com>
2022-11-09 21:08:45 +0700

    added function to make recycler view history work

--- app/src/main/java/com/eros/gestariwastebank/main/wallet/WalletFragment.kt
+++ app/src/main/java/com/eros/gestariwastebank/main/wallet/WalletFragment.kt
@@ -1,20 +1,44 @@
 package com.eros.gestariwastebank.main.wallet
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.eros.gestariwastebank.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.eros.gestariwastebank.data.Util
+import com.eros.gestariwastebank.databinding.FragmentWalletBinding
+import com.eros.gestariwastebank.main.wallet.history.HistoryAdapter
 
 
 class WalletFragment : Fragment() {
+
+    private lateinit var binding: FragmentWalletBinding
+    private lateinit var historyAdapter: HistoryAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wallet, container, false)
+        binding = FragmentWalletBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        historyAdapter = HistoryAdapter()
+
+        binding.rvHistory.adapter = historyAdapter
+
+        binding.rvHistory.layoutManager =
+            LinearLayoutManager(
+                context,
+                LinearLayoutManager.VERTICAL,
+                false
+            )
+
+        historyAdapter.setData(Util.allHistory)
     }
 }
