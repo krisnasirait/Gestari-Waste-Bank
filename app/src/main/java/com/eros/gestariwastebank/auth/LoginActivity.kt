3d9a2d5ed631ac5edb56ffb6a71805554959e857
Krisna Sirait <krisnapande00@outlook.com>
2022-11-11 12:50:32 +0700

    added prompt for login activity

--- app/src/main/java/com/eros/gestariwastebank/auth/LoginActivity.kt
+++ app/src/main/java/com/eros/gestariwastebank/auth/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.eros.gestariwastebank.auth
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eros.gestariwastebank.MainActivity
 import com.eros.gestariwastebank.R
 import com.eros.gestariwastebank.databinding.ActivityLoginBinding
 
@@ -14,9 +16,25 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+        setOnClickListener()
 
+        
+    }
+    
+    private fun setOnClickListener() {
         binding.tvRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+        // TODO: Prompt Should be added here 
+        binding.btnLogin.setOnClickListener {
+            val email = binding.etEmail.text
+            val password = binding.etPassword.text
+
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
         }
     }
 }
