cfe4c47f9744b9cad0a06b142f0073e340d826de
Krisna Sirait <krisnapande00@outlook.com>
2022-11-21 13:08:37 +0700

    create all fragment for pricelist and pricelist activity

--- app/src/main/java/com/eros/gestariwastebank/main/home/pricelist/PricelistActivity.kt
+++ app/src/main/java/com/eros/gestariwastebank/main/home/pricelist/PricelistActivity.kt
@@ -15,8 +15,6 @@ import com.eros.gestariwastebank.R
 import com.eros.gestariwastebank.databinding.ActivityPricelistBinding
 
 class PricelistActivity : AppCompatActivity() {
-
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityPricelistBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,34 +23,5 @@ class PricelistActivity : AppCompatActivity() {
         binding = ActivityPricelistBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.appBarPricelist.toolbar)
-
-        binding.appBarPricelist.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_pricelist)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.pricelist, menu)
-        return true
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_pricelist)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
