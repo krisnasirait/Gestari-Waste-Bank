0a76ff32fae3afebb3c518b8a21e247831dc3011
Krisna Sirait <krisnapande00@outlook.com>
2022-11-21 13:09:03 +0700

    optimize imports

--- app/src/main/java/com/eros/gestariwastebank/main/home/pricelist/PricelistActivity.kt
+++ app/src/main/java/com/eros/gestariwastebank/main/home/pricelist/PricelistActivity.kt
@@ -1,17 +1,7 @@
 package com.eros.gestariwastebank.main.home.pricelist
 
 import android.os.Bundle
-import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
-import com.eros.gestariwastebank.R
 import com.eros.gestariwastebank.databinding.ActivityPricelistBinding
 
 class PricelistActivity : AppCompatActivity() {
