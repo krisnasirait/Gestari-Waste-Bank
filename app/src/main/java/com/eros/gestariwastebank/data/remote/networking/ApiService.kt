5322d3b1eeb43c73316ab6cc3d51bd4ff3c73a0b
Krisna Sirait <krisnapande00@outlook.com>
2022-11-23 22:54:35 +0700

    added api service for all stuff

--- app/src/main/java/com/eros/gestariwastebank/data/remote/networking/ApiService.kt
+++ app/src/main/java/com/eros/gestariwastebank/data/remote/networking/ApiService.kt
@@ -16,6 +16,21 @@ interface ApiService {
     @GET("/katalog")
     suspend fun getCatalog() : CatalogResponse
 
+    @GET("/katalog/type/kertas")
+    suspend fun getCatalogPaper() : CatalogResponse
+
+    @GET("/katalog/type/plastik")
+    suspend fun getCatalogPlastic() : CatalogResponse
+
+    @GET("/katalog/type/logam")
+    suspend fun getCatalogMetal() : CatalogResponse
+
+    @GET("/katalog/type/kaca")
+    suspend fun getCatalogGlass() : CatalogResponse
+
+    @GET("/katalog/type/khusus")
+    suspend fun getCatalogOthers() : CatalogResponse
+
     @POST("user/login")
     fun loginUserApi(@Body loginRequest: LoginRequest): Call<LoginResponse>
 
