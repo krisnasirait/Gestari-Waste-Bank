8772a1c9d8652ee4a90cdb3c9470435338a7249d
Krisna Sirait <krisnapande00@outlook.com>
2022-11-23 11:29:04 +0700

    added login view model to view model factory

---
+++ app/src/main/java/com/eros/gestariwastebank/main/auth/viewmodel/LoginViewModel.kt
@@ -0,0 +1,42 @@
+package com.eros.gestariwastebank.main.auth.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.eros.gestariwastebank.data.model.login.Login
+import com.eros.gestariwastebank.data.remote.networking.request.LoginRequest
+import com.eros.gestariwastebank.domain.Repository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class LoginViewModel(
+    private val repository: Repository
+) : ViewModel() {
+
+
+    private val _login: MutableLiveData<Login?> = MutableLiveData()
+    val user: LiveData<Login?> = _login
+
+    private val _errorMessage : MutableLiveData<String> = MutableLiveData()
+    val errorMessage : LiveData<String> = _errorMessage
+
+    fun loginUser(loginRequest: LoginRequest) {
+        viewModelScope.launch {
+            kotlin.runCatching {
+                withContext(Dispatchers.IO){
+                    repository.loginUser(loginRequest)
+                }
+            }.onSuccess { data ->
+                withContext(Dispatchers.Main){
+                    _login.value = data.login
+                }
+            }.onFailure { error ->
+                withContext(Dispatchers.Main) {
+                    _errorMessage.value = error.message
+                }
+            }
+        }
+    }
+}
