600ac9026a67a1e62e1cd0e61cda036fd3db892d
Krisna Sirait <krisnapande00@outlook.com>
2022-11-24 11:42:05 +0700

    added metal view model and implement in in paper and plastic

--- app/src/main/java/com/eros/gestariwastebank/main/home/pricelist/ui/plastic/PlasticFragment.kt
+++ app/src/main/java/com/eros/gestariwastebank/main/home/pricelist/ui/plastic/PlasticFragment.kt
@@ -5,30 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.eros.gestariwastebank.databinding.FragmentMetalBinding
 import com.eros.gestariwastebank.databinding.FragmentPlasticBinding
+import com.eros.gestariwastebank.di.ViewModelFactory
+import com.eros.gestariwastebank.main.home.pricelist.ui.all.adapter.AllCatalogAdapter
 import com.eros.gestariwastebank.main.home.pricelist.ui.all.viewmodel.AllCatalogViewModel
+import com.eros.gestariwastebank.main.home.pricelist.ui.metal.viewmodel.MetalCatalogViewModel
+import com.eros.gestariwastebank.main.home.pricelist.ui.plastic.viewmodel.PlasticViewModel
 
 
 class PlasticFragment : Fragment() {
-    private var _binding: FragmentPlasticBinding? = null
+    private lateinit var binding: FragmentPlasticBinding
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    private lateinit var allCatalogAdapter: AllCatalogAdapter
+
+    private val viewModel: PlasticViewModel by activityViewModels(
+        factoryProducer = {
+            ViewModelFactory.getInstance(requireContext())
+        }
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentPlasticBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+        binding = FragmentPlasticBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        allCatalogAdapter = AllCatalogAdapter()
+        binding.rvFragmentPlastic.adapter = allCatalogAdapter
+        binding.rvFragmentPlastic.layoutManager = GridLayoutManager(context, 2)
+        viewModel.catalog.observe(requireActivity()) {
+            allCatalogAdapter.addAll(it!!)
+        }
+        viewModel.getCatalogPlastic()
     }
 }
