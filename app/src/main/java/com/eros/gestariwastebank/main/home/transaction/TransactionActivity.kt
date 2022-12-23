669754a8c1f3b4092ddb97fb65ff31dcf8fc2ade
Krisna Sirait <krisnapande00@outlook.com>
2022-12-23 12:36:15 +0700

    added transaction layout

---
+++ app/src/main/java/com/eros/gestariwastebank/main/home/transaction/TransactionActivity.kt
@@ -0,0 +1,12 @@
+package com.eros.gestariwastebank.main.home.transaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.eros.gestariwastebank.R
+
+class TransactionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_transaction)
+    }
+}
