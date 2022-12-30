68a82bab4e15ef02bb16649690f4068c8f350526
Krisna Sirait <krisnapande00@outlook.com>
2022-12-30 13:55:57 +0700

    added function delete by id

--- app/src/main/java/com/eros/gestariwastebank/data/local/room/CartDao.kt
+++ app/src/main/java/com/eros/gestariwastebank/data/local/room/CartDao.kt
@@ -21,12 +21,15 @@ interface CartDao {
     @Query("INSERT OR REPLACE INTO Cart(id, name, itemImage, amount, price) VALUES (:id, :name, :itemImage, COALESCE((SELECT amount FROM Cart WHERE id = :id), 0) + :amount, :price)")
     fun addOrInsertById(id: Int, name: String, itemImage: String, amount: Int, price: Int)
 
-    @Query("SELECT COUNT(*) FROM customers")
+    @Query("SELECT COUNT(*) FROM cart")
     fun getRowCount(): Int
 
-    @Query("UPDATE Cart SET amount = amount - 1 WHERE id = :id AND amount >= 1; DELETE FROM Cart WHERE id = :id AND amount < 1")
-    fun decrementQuantity(productId: Int)
+    @Query("UPDATE cart SET amount = amount - 1 WHERE id = :id AND amount >= 1")
+    fun decrementQuantity(id: Int)
 
-    @Query("UPDATE Cart SET amount = amount + 1 WHERE id = :id")
-    fun incrementQuantity(productId: Int)
+    @Query("DELETE FROM cart WHERE id = :id AND amount < 1")
+    fun deleteById(id: Int)
+
+    @Query("UPDATE cart SET amount = amount + 1 WHERE id = :id")
+    fun incrementQuantity(id: Int)
 }
