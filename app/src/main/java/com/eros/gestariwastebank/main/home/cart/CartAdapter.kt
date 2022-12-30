c9c24ea913b2a1f6324c06126697780436acfb9f
Krisna Sirait <krisnapande00@outlook.com>
2022-12-30 14:07:22 +0700

    add rv item clickable function in cart adapter

--- app/src/main/java/com/eros/gestariwastebank/main/home/cart/CartAdapter.kt
+++ app/src/main/java/com/eros/gestariwastebank/main/home/cart/CartAdapter.kt
@@ -7,33 +7,52 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.eros.gestariwastebank.data.model.cart.Cart
 import com.eros.gestariwastebank.databinding.RvItemCartBinding
+import io.reactivex.subjects.PublishSubject
 import java.text.NumberFormat
 import java.util.*
 
-class CartAdapter : RecyclerView.Adapter<CartAdapter.ViewHolder>() {
+class CartAdapter : RecyclerView.Adapter<CartAdapter.CartViewHolder>() {
 
-    private lateinit var binding: RvItemCartBinding
     private val itemCart = mutableListOf<Cart?>()
+    private val clickSubject = PublishSubject.create<Cart>()
+    val clickEvent: io.reactivex.Observable<Cart> = clickSubject
 
-    inner class ViewHolder(private val itemBinding: RvItemCartBinding) :
+    inner class CartViewHolder(
+        val itemBinding: RvItemCartBinding
+    ) :
         RecyclerView.ViewHolder(itemBinding.root) {
         fun bind(cart: Cart) {
             itemBinding.tvItemName.text = cart.name
-            itemBinding.tvPrice.text = "Rp " + NumberFormat.getNumberInstance(Locale.US).format(cart.price?.toInt()).toString()
-            Glide.with(binding.root)
+            itemBinding.tvPrice.text =
+                "Rp " + NumberFormat.getNumberInstance(Locale.US).format(cart.price?.toInt())
+                    .toString()
+            Glide.with(itemBinding.root)
                 .load(cart.itemImage)
                 .into(itemBinding.ivItem)
-            itemBinding.tvQuantity.text = "Quantity : ${cart.amount} kg"
+            itemBinding.tvTrashAmount.text = "${cart.amount}"
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        binding = RvItemCartBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return ViewHolder(binding)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartViewHolder {
+        return CartViewHolder(
+            RvItemCartBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        itemCart[position]?.let { holder.bind(it) }
+    override fun onBindViewHolder(holder: CartViewHolder, position: Int) {
+        itemCart[position]?.let {
+            holder.bind(it)
+            holder.itemBinding.btnMinus.setOnClickListener {
+                clickSubject.onNext(itemCart[position]!!)
+            }
+            holder.itemBinding.btnPlus.setOnClickListener {
+                clickSubject.onNext(itemCart[position]!!)
+            }
+        }
     }
 
     override fun getItemCount(): Int {
